48a16e2cc439745170516eee453cf68e7b814794
Aarul Kumar <aarulk0110@gmail.com>
2026-05-09 17:05:49 +0530

    Phase 3B

--- controller/listings.js
+++ controller/listings.js
@@ -1,4 +1,8 @@
 const Listing = require("../models/listing.js");
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const mapToken = process.env.MAP_TOKEN;
+const geocodingClient = mbxGeocoding({ accessToken: mapToken });
+
 module.exports.index = async (req, res) => {
     const allListings = await Listing.find();
     res.render("./listings/index.ejs", { allListings });
@@ -19,12 +23,19 @@ module.exports.showListing = async (req, res) => {
 };
 
 module.exports.createListing = async (req, res) => {
+    let response = await geocodingClient.forwardGeocode({
+        query: req.body.listing.location,
+        limit: 1
+    }).send();
+
     let url = req.file.path;
     let filename = req.file.filename;
     const newListing = new Listing(req.body.listing);
     newListing.owner = req.user._id;
     newListing.image = { url, filename };
-    await newListing.save();
+    newListing.geometry = response.body.features[0].geometry;
+    let savedListing = await newListing.save();
+    console.log(savedListing);
     req.flash("success", "New listing created");
     res.redirect("/listings");
 };
@@ -32,12 +43,24 @@ module.exports.createListing = async (req, res) => {
 module.exports.renderEditForm = async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
-    res.render("listings/edit.ejs", { listing });
+    if (!listing) {
+        req.flash("error", "Listing does not exist");
+        return res.redirect("/listings");
+    }
+    let originalImageUrl = listing.image.url;
+    originalImageUrl = originalImageUrl.replace("/upload", "/upload/w_250");
+    res.render("listings/edit.ejs", { listing, originalImageUrl });
 };
 
 module.exports.updateListing = async (req, res) => {
     let { id } = req.params;
-    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    let listing = await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    if (typeof req.file != "undefined") {
+        let url = req.file.path;
+        let filename = req.file.filename;
+        listing.image = { url, filename };
+        await listing.save();
+    }
     req.flash("success", "Listing updated");
     res.redirect(`/listings/${id}`);
 };
---
+++ public/js/map.js
@@ -0,0 +1,10 @@
+mapboxgl.accessToken=mapToken;
+const map=new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/standard',
+    projection: 'globe',
+    zoom: 10,
+    center: listing.geometry.coordinates
+});
+
+const marker=new mapboxgl.Marker({ color: 'red' }).setLngLat(listing.geometry.coordinates).setPopup(new mapboxgl.Popup({ offset: 25 }).setHTML(`<h4>${listing.title}</h4><p>Exact location will beprovided after booking</p>`)).addTo(map);
